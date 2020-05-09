f8e6ebe2686d5d57437113b026b1c5668ae00e8c
heqinglin <heqinglin@52tt.com>
2020-05-09 12:14:56 +0800

    update

--- demoapp/src/main/java/com/sankuai/waimai/router/demo/test/UriAnnotationInit_test_lib1.java
+++ demoapp/src/main/java/com/sankuai/waimai/router/demo/test/UriAnnotationInit_test_lib1.java
@@ -5,7 +5,7 @@ import com.sankuai.waimai.router.common.UriAnnotationHandler;
 
 public class UriAnnotationInit_test_lib1 implements IUriAnnotationInit {
   public void init(UriAnnotationHandler handler) {
-//    handler.register("", "", "/lib1", "com.sankuai.waimai.router.demo.lib1.basic.DemoLibActivity1", false);
-    handler.register("lib1_scheme", "lib1_host", "/exported", "com.sankuai.waimai.router.demo.lib1.basic.DemoLibActivity2", true);
+    handler.register("", "", "/lib1", "com.sankuai.waimai.router.demo.lib1.basic.DemoLibActivity1", false);
+    handler.register("lib1_scheme", "lib1_host", "/exported", "com.sankuai.waimai.router.demo.lib1.basic.DemoLibActivity2", false);
   }
 }
