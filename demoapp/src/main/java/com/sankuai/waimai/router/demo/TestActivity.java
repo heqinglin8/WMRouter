5a4462cc80bf48ff3501d9726288ff269e0ebc31
heqinglin <heqinglin@52tt.com>
2021-01-15 17:01:59 +0800

    完成编译插件的配置修改，解决编译不过去的问题

--- demoapp/src/main/java/com/sankuai/waimai/router/demo/TestActivity.java
+++ demoapp/src/main/java/com/sankuai/waimai/router/demo/TestActivity.java
@@ -1,14 +1,14 @@
 package com.sankuai.waimai.router.demo;
 
-import io.flutter.embedding.android.FlutterActivity;
+//import io.flutter.embedding.android.FlutterActivity;
 
-public class TestActivity extends FlutterActivity {
-
-//    public static CachedEngineIntentBuilder withCachedEngine(@NonNull String cachedEngineId) {
-//        return new CachedEngineIntentBuilder(TestActivity.class, cachedEngineId);
+//public class TestActivity extends FlutterActivity {
+//
+////    public static CachedEngineIntentBuilder withCachedEngine(@NonNull String cachedEngineId) {
+////        return new CachedEngineIntentBuilder(TestActivity.class, cachedEngineId);
+////    }
+//    public static NewEngineIntentBuilder withNewEngine() {
+//        return new NewEngineIntentBuilder(TestActivity.class);
 //    }
-    public static NewEngineIntentBuilder withNewEngine() {
-        return new NewEngineIntentBuilder(TestActivity.class);
-    }
-
-}
+//
+//}
